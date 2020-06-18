2dd90f0e2d9e48287af874614ab0e9e50459cae4
lalalawson <lawson.tkw@gmail.com>
2020-06-19 01:07:26 +0800

    added firebase for android as well as user auth using email

--- lib/login_page/credentials_page.dart
+++ lib/login_page/credentials_page.dart
@@ -1,105 +1,141 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:purrductive/const/colors.dart';
 import 'package:purrductive/const/components.dart';
 import 'package:purrductive/const/routeNames.dart';
 
-class CredentialsPage extends StatelessWidget {
+class CredentialsPage extends StatefulWidget {
+  @override
+  _CredentialsPageState createState() => _CredentialsPageState();
+}
+
+class _CredentialsPageState extends State<CredentialsPage> {
+  String email;
+  String password;
+  bool showSpinner = false;
+
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: silverwhite,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TyperAnimatedTextKit(
-              speed: Duration(milliseconds: 200),
-              pause: Duration(milliseconds: 1800),
-              isRepeatingAnimation: true,
-              text: ['purrductive'],
-              textStyle: TextStyle(
-                fontFamily: 'pixelsix',
-                fontSize: 50.0,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TyperAnimatedTextKit(
+                speed: Duration(milliseconds: 200),
+                pause: Duration(milliseconds: 1800),
+                isRepeatingAnimation: true,
+                text: ['purrductive'],
+                textStyle: TextStyle(
+                  fontFamily: 'pixelsix',
+                  fontSize: 50.0,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              style: TextStyle(
-                fontFamily: 'pixelmix',
-                fontSize: 18.0,
-                fontWeight: FontWeight.w300,
+              SizedBox(
+                height: 20.0,
               ),
-              textAlign: TextAlign.center,
-              decoration: kTextFieldInputDecoration.copyWith(
-                hintText: 'username',
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              obscureText: true,
-              style: TextStyle(
-                fontFamily: 'pixelmix',
-                fontSize: 18.0,
-                fontWeight: FontWeight.w300,
-              ),
-              textAlign: TextAlign.center,
-              decoration: kTextFieldInputDecoration.copyWith(
-                hintText: 'password',
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, registrationPage);
-              },
-              child: Container(
-                child: Text(
-                  'new here? click here to sign up',
-                  style: TextStyle(fontFamily: 'pixelmix', fontSize: 12.0),
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                style: TextStyle(
+                  fontFamily: 'pixelmix',
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.center,
+                decoration: kTextFieldInputDecoration.copyWith(
+                  hintText: 'email',
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            SizedBox(
-              width: 200.0,
-              child: RaisedButton(
-                color: cyan,
-                elevation: 5.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(
-                    width: 4,
-                    color: darkCyan,
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  password = value;
+                },
+                obscureText: true,
+                style: TextStyle(
+                  fontFamily: 'pixelmix',
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.center,
+                decoration: kTextFieldInputDecoration.copyWith(
+                  hintText: 'password',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, registrationPage);
+                },
+                child: Container(
+                  child: Text(
+                    'new here? click here to sign up',
+                    style: TextStyle(fontFamily: 'pixelmix', fontSize: 12.0),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, homeScreen);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'pixelsix',
-                      color: Colors.black,
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              SizedBox(
+                width: 200.0,
+                child: RaisedButton(
+                  color: cyan,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(
+                      width: 4,
+                      color: darkCyan,
+                    ),
+                  ),
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.pushNamed(context, homeScreen);
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'pixelsix',
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
